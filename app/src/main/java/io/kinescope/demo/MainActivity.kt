e3021e83073ca142d92a474d6242a984c2d71830
Ilya Zyuzin <beceji9iu@gmail.com>
2022-08-12 16:32:20 +0300

    UI changes

--- app/src/main/java/io/kinescope/demo/MainActivity.kt
+++ app/src/main/java/io/kinescope/demo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.kinescope.sdk.network.NetworkModule
 import io.kinescope.sdk.network.Repository
+import io.kinescope.sdk.player.KinescopePlayer
 import io.kinescope.sdk.view.KinescopePlayerView
 
 class MainActivity : AppCompatActivity() {
@@ -14,10 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        val player = findViewById<KinescopePlayerView>(R.id.kinescope_player)
-
-        player?.setMediaUrl("https://msk-1-storage.kinescope.io/mp4/65659012-b489-4505-9a74-f3d12e0ed656/08f8b309-97b0-4550-aed6-ccd751be5f79")
-        player?.play()
+        val playerView = findViewById<KinescopePlayerView>(R.id.kinescope_player)
+        val player = KinescopePlayer(this)
+        playerView.bindPlayer(player)
+        playerView?.setMediaUrl("https://msk-1-storage.kinescope.io/mp4/65659012-b489-4505-9a74-f3d12e0ed656/08f8b309-97b0-4550-aed6-ccd751be5f79")
+        player.play()
 
         //Repository.getVideo("41e3f776-90c1-4ea8-8bf7-e2fe774eaa69")
         Repository.getAll()
