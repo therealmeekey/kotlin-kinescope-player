0e7e4911f015705741a2b2701482375d9d478a43
Ilya Zyuzin <beceji9iu@gmail.com>
2022-08-16 19:22:25 +0300

    Player and player UI changes

--- Kinescope Player SDK/src/main/java/io/kinescope/sdk/player/KinescopePlayer.kt
+++ Kinescope Player SDK/src/main/java/io/kinescope/sdk/player/KinescopePlayer.kt
@@ -1,21 +1,40 @@
 package io.kinescope.sdk.player
 
-class KinescopePlayer {
+import android.content.Context
+import com.google.android.exoplayer2.ExoPlayer
+import io.kinescope.sdk.logger.KinescopeLogger
 
+class KinescopePlayer(context:Context) {
 
+    var exoPlayer: ExoPlayer? = null
 
-
+    init {
+        exoPlayer = ExoPlayer.Builder(context).build()
+    }
 
 
     fun play() {
-
+        exoPlayer?.play()
+        KinescopeLogger.log("Play")
     }
 
     fun pause() {
-
+        exoPlayer?.pause()
+        KinescopeLogger.log("Pause")
     }
 
     fun stop() {
+        exoPlayer?.stop()
+        KinescopeLogger.log("Stop")
+    }
 
+    fun seekTo(toMilliSeconds:Long) {
+        exoPlayer?.seekTo(toMilliSeconds)
+        KinescopeLogger.log("seek to ${toMilliSeconds / 1000} seconds" )
+    }
+
+    fun setPlaybackSpeed(speed:Float) {
+        exoPlayer?.setPlaybackSpeed(speed)
+        KinescopeLogger.log("Playback speed changed to $speed")
     }
 }
--- Kinescope Player SDK/src/main/java/io/kinescope/sdk/view/KinescopePlayerView.kt
+++ Kinescope Player SDK/src/main/java/io/kinescope/sdk/view/KinescopePlayerView.kt
@@ -2,47 +2,116 @@ package io.kinescope.sdk.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.ImageButton
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackParameters
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.ui.PlayerView.SHOW_BUFFERING_ALWAYS
 import com.google.android.exoplayer2.ui.StyledPlayerView
 import io.kinescope.sdk.R
+import io.kinescope.sdk.logger.KinescopeLogger
+import io.kinescope.sdk.player.KinescopePlayer
 
-class KinescopePlayerView(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs), KinescopePlayerViewInterface {
+class KinescopePlayerView(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
 
-    var player: ExoPlayer? = null
-    var playerView: StyledPlayerView? = null
+    companion object {
+        fun switchTargetView(oldPlayerView:KinescopePlayerView, newPlayerView:KinescopePlayerView, player:KinescopePlayer) {
+            //StyledPlayerView.switchTargetView()
+        }
+    }
+
+    var kinescopePlayer: KinescopePlayer? = null
+    var exoPlayerView: StyledPlayerView? = null
 
 
     init {
         inflate(context, R.layout.view_kinesope_player, this)
-        player = ExoPlayer.Builder(context).build()
-        playerView = findViewById(R.id.v_exoplayer)
-        playerView?.player = player
+        exoPlayerView = findViewById(R.id.v_exoplayer)
+        exoPlayerView?.setShowBuffering(StyledPlayerView.SHOW_BUFFERING_WHEN_PLAYING)
+        setUIlisteners()
     }
 
-    fun intExoPlayer() {
-
+    fun bindPlayer(kinescopePlayer: KinescopePlayer) {
+        this.kinescopePlayer = kinescopePlayer
+        exoPlayerView?.player = kinescopePlayer.exoPlayer
     }
+
 
     fun setMediaUrl(url:String) {
-        player?.setMediaItem(MediaItem.fromUri(url))
-        player?.playWhenReady = false
-        player?.prepare()
+        kinescopePlayer?.exoPlayer?.setMediaItem(MediaItem.fromUri(url))
+        kinescopePlayer?.exoPlayer?.playWhenReady = false
+        kinescopePlayer?.exoPlayer?.prepare()
     }
 
 
-    override fun play() {
-        player?.play()
+    /*override fun play() {
+        kineScopePlayer?.play()
     }
 
     override fun pause() {
-        player?.pause()
+        kineScopePlayer?.pause()
     }
 
     override fun stop() {
-        player?.stop()
+        kineScopePlayer?.stop()
+    }
+
+    fun seek(toMilliSeconds:Long) {
+        kineScopePlayer?.seekTo(toMilliSeconds)
+    }
+
+    fun setPlaybackSpeed(speed:Float) {
+        kineScopePlayer?.setPlaybackSpeed(speed)
+    }*/
+
+    fun setUIlisteners() {
+        /*playerView?.findViewById<ImageButton>(R.id.kinescope_play)?.setOnClickListener {
+            if (player!!.isPlaying) {
+                pause()
+            }
+            else {
+                play()
+            }
+        }*/
+
+        exoPlayerView?.setFullscreenButtonClickListener {
+
+        }
+
+        kinescopePlayer?.exoPlayer?.addListener(object :Player.Listener {
+            override fun onVolumeChanged(volume: Float) {
+                super.onVolumeChanged(volume)
+                KinescopeLogger.log("Volume changed to: $volume")
+            }
+
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+            }
+
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                super.onIsPlayingChanged(isPlaying)
+                KinescopeLogger.log("Is playing: $isPlaying")
+            }
+
+            override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters) {
+                super.onPlaybackParametersChanged(playbackParameters)
+                KinescopeLogger.log("Playback speed changed to $playbackParameters.speed")
+            }
+
+            override fun onPositionDiscontinuity(
+                oldPosition: Player.PositionInfo,
+                newPosition: Player.PositionInfo,
+                reason: Int
+            ) {
+                oldPosition
+                newPosition
+                super.onPositionDiscontinuity(oldPosition, newPosition, reason)
+            }
+
+        })
     }
 
 }
