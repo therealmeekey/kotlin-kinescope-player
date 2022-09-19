4b160109104b4b69a30bd33a5fee6ef0abf0f913
Ilya Zyuzin <beceji9iu@gmail.com>
2022-09-19 21:12:12 +0300

    Demo app updated with video list

--- app/src/main/java/io/kinescope/demo/MainActivity.kt
+++ app/src/main/java/io/kinescope/demo/MainActivity.kt
@@ -2,6 +2,9 @@ package io.kinescope.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import io.kinescope.sdk.models.common.KinescopeAllVideosResponse
 import io.kinescope.sdk.network.NetworkModule
 import io.kinescope.sdk.network.Repository
 import io.kinescope.sdk.player.KinescopePlayer
@@ -16,13 +19,30 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         val playerView = findViewById<KinescopePlayerView>(R.id.kinescope_player)
+        val videosView = findViewById<RecyclerView>(R.id.rv_videos)
         val player = KinescopePlayer(this)
         playerView.setPlayer(player)
-        playerView?.setMediaUrl("https://msk-2-storage.kinescope.io/mp4/ff7c8e12-c59f-4ea0-8f0d-8a04959dfcc3/58bf17d8-c03a-4097-a450-ecde5f8c377a")
-        player.play()
+
+        val adapter = VideosAdapter() {
+            player.setVideo(it)
+            player.play()
+        }
+
+        videosView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        videosView.adapter = adapter
+        //playerView?.setMediaUrl("https://msk-2-storage.kinescope.io/mp4/ff7c8e12-c59f-4ea0-8f0d-8a04959dfcc3/58bf17d8-c03a-4097-a450-ecde5f8c377a")
+        //player.play()
 
         //Repository.getVideo("41e3f776-90c1-4ea8-8bf7-e2fe774eaa69")
-        Repository.getAll()
+        Repository.getAll(object : Repository.GetAllVideosCallback {
+            override fun onResponse(value: KinescopeAllVideosResponse) {
+                adapter.updateData(value.data)
+            }
+
+            override fun onFailure() {
+
+            }
+        })
     }
 
 
---
+++ app/src/main/java/io/kinescope/demo/VideosAdapter.kt
@@ -0,0 +1,51 @@
+package io.kinescope.demo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.kinescope.sdk.models.videos.KinescopeVideo
+
+class VideosAdapter(val callback: (KinescopeVideo)-> Unit) : RecyclerView.Adapter<VideosAdapter.ViewHolder>() {
+
+    private val arr = ArrayList<KinescopeVideo>()
+
+    fun updateData(value:List<KinescopeVideo>) {
+        arr.clear()
+        arr.addAll(value)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideosAdapter.ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_video, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: VideosAdapter.ViewHolder, position: Int) {
+            holder.bind(arr[position])
+    }
+
+    override fun getItemCount(): Int {
+        return arr.size
+    }
+
+    inner class ViewHolder(private val view:View) :  RecyclerView.ViewHolder(view.rootView), View.OnClickListener {
+        private var video: KinescopeVideo? = null
+        val title:TextView = view.findViewById(R.id.tv_title)
+        //val title:TextView = view.findViewById(R.id.tv_title)
+
+        init {
+            view.setOnClickListener(this)
+        }
+
+        fun bind(video: KinescopeVideo) {
+            this.video = video
+            title.text = video.title
+        }
+
+        override fun onClick(p0: View?) {
+            callback.invoke(video!!)
+        }
+
+    }
+}
