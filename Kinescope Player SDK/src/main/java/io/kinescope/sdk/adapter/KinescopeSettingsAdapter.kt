d462f05d43ef329920f7c44630da4ec53d2cbc72
Ilya Zyuzin <beceji9iu@gmail.com>
2022-10-24 19:10:39 +0300

    Settings list updated

--- Kinescope Player SDK/src/main/java/io/kinescope/sdk/adapter/KinescopeSettingsAdapter.kt
+++ Kinescope Player SDK/src/main/java/io/kinescope/sdk/adapter/KinescopeSettingsAdapter.kt
@@ -3,11 +3,13 @@ package io.kinescope.sdk.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import io.kinescope.sdk.R
 
-class KinescopeSettingsAdapter(private val options:Array<String>, private val callback:((String) -> Unit)?) : RecyclerView.Adapter<KinescopeSettingsAdapter.ViewHolder>() {
+class KinescopeSettingsAdapter(private val options:Array<String>, private var activeOption:String?, private val callback:((String) -> Unit)?) : RecyclerView.Adapter<KinescopeSettingsAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KinescopeSettingsAdapter.ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_option, parent, false))
     }
@@ -20,16 +22,20 @@ class KinescopeSettingsAdapter(private val options:Array<String>, private val ca
         return options.size
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view.rootView), View.OnClickListener {
+    inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view.rootView), View.OnClickListener {
         private var option:String? = null
         private val optionView:TextView = view.findViewById(R.id.tv_option)
+        private val optionMarkView:ImageView = view.findViewById(R.id.iv_option_mark)
 
         fun bind(value:String) {
             this.option = value
             optionView.text = option
+            view.setOnClickListener(this)
+            optionMarkView.isVisible = this.option == activeOption
         }
 
         override fun onClick(v: View?) {
+            activeOption = this.option!!
             callback?.invoke(option!!)
         }
     }
