c16334cde4729b0a4dd789ad5f3e055587a1ceb7
Ilya Zyuzin <beceji9iu@gmail.com>
2022-11-06 23:08:06 +0300

    Model for player options

---
+++ Kinescope Player SDK/src/main/java/io/kinescope/sdk/player/KinescopePlayerOptions.kt
@@ -0,0 +1,9 @@
+package io.kinescope.sdk.player
+
+data class KinescopePlayerOptions (
+    val showFullscreenButton:Boolean = true,
+    val showSubtitlesButton:Boolean = false,
+    val showQualityButton:Boolean = false,
+    val showSeekBar:Boolean = true,
+    val showDuration:Boolean = true
+        )
