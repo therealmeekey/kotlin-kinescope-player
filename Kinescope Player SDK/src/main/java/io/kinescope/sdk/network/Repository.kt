4363777b7299ffd144cdf8436f125546ca123f27
Ilya Zyuzin <beceji9iu@gmail.com>
2022-11-07 14:21:43 +0300

    API repository getVideo updated

--- Kinescope Player SDK/src/main/java/io/kinescope/sdk/network/Repository.kt
+++ Kinescope Player SDK/src/main/java/io/kinescope/sdk/network/Repository.kt
@@ -3,6 +3,7 @@ package io.kinescope.sdk.network
 import io.kinescope.sdk.logger.KinescopeLogger
 import io.kinescope.sdk.models.common.KinescopeAllVideosResponse
 import io.kinescope.sdk.models.common.KinescopeMetaResponse
+import io.kinescope.sdk.models.common.KinescopeResponse
 import io.kinescope.sdk.models.pagination.KinescopeMetaData
 import io.kinescope.sdk.models.videos.KinescopeVideo
 import retrofit2.Call
@@ -10,17 +11,19 @@ import retrofit2.Callback
 import retrofit2.Response
 
 object Repository {
-    fun getVideo(value:String) {
-        NetworkModule.getVideoApi().getVideo(value).enqueue(object : Callback<KinescopeVideo> {
+    fun getVideo(value: String, callback: GetVideoCallback) {
+        NetworkModule.getVideoApi().getVideo(value).enqueue(object : Callback<KinescopeResponse<KinescopeVideo>> {
             override fun onResponse(
-                call: Call<KinescopeVideo>,
-                response: Response<KinescopeVideo>
+                call: Call<KinescopeResponse<KinescopeVideo>>,
+                response: Response<KinescopeResponse<KinescopeVideo>>
             ) {
+                callback.onResponse(response.body()!!.data)
                 KinescopeLogger.log("SUCCESS")
             }
 
-            override fun onFailure(call: Call<KinescopeVideo>, t: Throwable) {
+            override fun onFailure(call: Call<KinescopeResponse<KinescopeVideo>>, t: Throwable) {
                 KinescopeLogger.log("FAILURE")
+                callback.onFailure()
             }
 
         })
@@ -45,6 +48,11 @@ object Repository {
 
     interface GetAllVideosCallback {
         fun onResponse(value:KinescopeAllVideosResponse)
+        fun onFailure()
+    }
+
+    interface GetVideoCallback {
+        fun onResponse(value:KinescopeVideo)
         fun onFailure()
     }
 
