02232043872d2e79a4d4ed5b3a4a823bfc031512
Ilya Zyuzin <beceji9iu@gmail.com>
2022-11-14 19:10:32 +0300

    Demo app updated with new test activities

---
+++ app/src/main/java/io/kinescope/demo/customui/CustomUIActivity.kt
@@ -0,0 +1,6 @@
+package io.kinescope.demo.customui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class CustomUIActivity : AppCompatActivity() {
+}
---
+++ app/src/main/java/io/kinescope/demo/fullscreen/FullscreenActivity.kt
@@ -0,0 +1,6 @@
+package io.kinescope.demo.fullscreen
+
+import androidx.appcompat.app.AppCompatActivity
+
+class FullscreenActivity : AppCompatActivity() {
+}
